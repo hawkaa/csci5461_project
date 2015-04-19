7fe8834e24cabb2f81631cc478e2fd3bba375dd2
Håkon Åmdal <hakon@aamdal.com>
2015-04-18 21:16:45 -0500

    add code for creating coexpression network

---
+++ coex.py
@@ -0,0 +1,20 @@
+import camoco as co
+
+# read refgen
+ZMFGS = co.RefGen("Zm5bFGS")
+
+# create KLS network
+ZmTissueNetwork = co.COB.from_table(
+       'split/KLS.txt',
+       'KLS', # Dataset Name
+       'Co-expression network for all KLS annotated samples', # Short Description
+       ZMFGS, #A RefGen instance
+       rawtype='RNASEQ', # Expression datatype, either 'RNASEQ' or 'MICROARRAY'
+       max_gene_missing=0.4, # See Expr._quality_control
+       min_expr=0.1,  # See Expr._quality_control
+       quantile=False,  # See Expr._quality_control
+       dry_run=False,  # See Expr._quality_control
+       sep=',', # table is comma seperated
+       max_val=300, # See Expr._normalize
+)
+
