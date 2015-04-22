b299805895a97181ea8130f3b394e204e40a2d00
Håkon Åmdal <hakon@aamdal.com>
2015-04-21 23:23:40 -0500

    dump degree script

---
+++ dump_degree.py
@@ -0,0 +1,23 @@
+from optparse import OptionParser
+import camoco as co
+import pandas as pd
+
+parser = OptionParser()
+parser.add_option("-n", "--network-name", help="Name of network", action="store",
+                    type="string", dest="network_name")
+
+def main():
+    (options, args) = parser.parse_args()
+    if not options.network_name: parser.error("Must specify name of network")
+
+    network = co.COB(options.network_name)
+    degree = network.degree
+    
+    i = 0
+    for index, row in degree.iterrows():
+        msg = "%s\t%s\t%s" % (i, index, row["Degree"])
+        print(msg)
+        i += 1
+
+if __name__ == "__main__":
+    main()
